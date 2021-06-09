67feade0a984f19184d2c0f9c1ce4bb490d670dc
ishaansharma7 <ishaansharma711@gmail.com>
2021-06-09 16:28:49 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id:1,
+        text: 'Doctors appointment',
+        day: 'Feb 5th at 2:30',
+        reminder: true,
+    },
+    {
+        id:2,
+        text: 'Assignment',
+        day: 'Feb 4th at 12:30',
+        reminder: true,
+    },
+    {
+        id:3,
+        text: 'Clean Room',
+        day: 'Feb 7th at 4:30',
+        reminder: false,
+    },
+])
+// delete task
+  const deleteTask = (id) =>{
+    console.log('delete')
+    setTasks(tasks.filter((task) => task.id !== id))
+}
+// toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* <Header title="Hello World"/> */}
+      <Header />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No task'}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+// const Header = (props) => {
+const Header = ({title}) => {
+
+    return (
+        <div>
+            <header className='header'>
+                {/* <h1>{props.title}</h1> */}
+                <h1>{title}</h1>
+                <Button color='green' text='Add'/>
+            </header>
+        </div>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,  // generate error in dev mode if wrong type
+}
+
+// CSS in JS
+// style={{backgroundColor: color}} this is an attribute
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'black',
+// }
+
+export default Header
